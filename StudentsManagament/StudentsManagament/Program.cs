c89357e675f5e2a7f194bb62cebf434a384af526
ericka.viraca <evDev02@gmail.com>
2020-03-23 18:25:28 -0400

    Implement the main method class.

---
+++ StudentsManagament/StudentsManagament/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace StudentsManagament
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                Runner runner = Runner.GetInstance();
+                string csvFile = $"{args[0]}";
+                await runner.InsertInitialData(csvFile);
+                await runner.ExecuteCommand(args);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception occured: " + e.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
