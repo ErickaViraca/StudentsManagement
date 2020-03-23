24d68f2438b5488350f848a5769d5218b711a5c0
ericka.viraca <evDev02@gmail.com>
2020-03-23 18:19:02 -0400

    Implemented student model.

---
+++ StudentsManagament/StudentsManagament/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentsManagament.Models
+{
+    public class Student
+    {
+        public string studentType { get; set; }
+        public string studentName { get; set; }
+        public string gender { get; set; }
+        public string timestamp { get; set; }
+    }
+}
